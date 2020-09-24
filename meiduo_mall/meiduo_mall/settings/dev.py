77060cbf2d8fb5e623e3204c5ba0173472c28228
15610950227 <https://gitee.com/zxf1242038234>
2020-09-24 23:59:39 +0800

    创建apps,用户文件，所有数据完成后再进行迁移

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os
+import os,sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework', #rest_framework
+
+    'users.apps.UsersConfig', #用户
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -196,3 +198,5 @@ REST_FRAMEWORK = {
     # 异常处理
     'EXCEPTION_HANDLER': 'meiduo_mall.utils.exceptions.exception_handler',
 }
+#用户模型
+AUTH_USER_MODEL = 'users.User'
